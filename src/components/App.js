616ff4ad5c28ab0c6329c132055fea3d73d1316b
seanpara <spara77@gmail.com>
2019-01-29 16:46:56 -0500

    added oauth and redux form

--- src/components/App.js
+++ src/components/App.js
@@ -13,11 +13,11 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Header/>
-          <Route path='/' exact component={StreamList}></Route>
-          <Route path='/streams/new' exact component={StreamCreate}></Route>
-          <Route path='/streams/edit' exact component={StreamEdit}></Route>
-          <Route path='/streams/delete' exact component={StreamDelete}></Route>
-          <Route path='/streams/show' exact component={StreamShow}></Route>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact render={() => <StreamCreate/>} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
