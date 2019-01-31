72754788268c9b7c7c098280a105f158a31f9ae0
seanpara <spara77@gmail.com>
2019-01-31 18:03:14 -0500

    first commit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import { Field, reduxForm } from 'redux-form'
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends Component {
 
@@ -28,8 +30,8 @@ class StreamCreate extends Component {
      )
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues)
   }
 
   render() {
@@ -61,7 +63,9 @@ const validate = (formValues) => {
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate)
+
+export default connect(null, { createStream })(formWrapped)
